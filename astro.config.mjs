876f94a2f01ac36f409ae360e3e5226af1bb5f11
irvin373 <irvin373@gmail.com>
2026-02-10 21:12:04 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
+// import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://irvin373.github.io',
+  base: '/siendo-sinergia',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  // integrations: [react()], // Integración de React
+  server: {
+    open: true, // 🚀 Esto hace que se abra el navegador automáticamente
+  },
 });
